f7ad012b137e1e5df244b835647d399f024843fc
Bonneton Corentin <contact@titin.fr>
2025-03-16 01:36:35 +0100

    Update ToastServiceProvider.php

--- src/ToastServiceProvider.php
+++ src/ToastServiceProvider.php
@@ -24,31 +24,39 @@ class ToastServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__ . '/../resources/views' => resource_path('views/vendor/toasts'),
-        ],[
+        ], [
             'livewire-toasts'
         ]);
     }
 
     public function configureLivewire(): void
     {
-        Component::macro('success', fn($message) => $this->dispatch('toast', [
+        Component::macro('success', fn($message, $title = '', $delay = 5000) => $this->dispatch('toast', [
             'type' => ToastTypeEnum::SUCCESS,
             'message' => $message,
+            'title' => $title,
+            'delay' => $delay
         ]));
 
-        Component::macro('error', fn($message) => $this->dispatch('toast', [
+        Component::macro('error', fn($message, $title = '', $delay = 5000) => $this->dispatch('toast', [
             'type' => ToastTypeEnum::ERROR,
             'message' => $message,
+            'title' => $title,
+            'delay' => $delay
         ]));
 
-        Component::macro('warning', fn($message) => $this->dispatch('toast', [
+        Component::macro('warning', fn($message, $title = '', $delay = 5000) => $this->dispatch('toast', [
             'type' => ToastTypeEnum::WARNING,
             'message' => $message,
+            'title' => $title,
+            'delay' => $delay
         ]));
 
-        Component::macro('information', fn($message) => $this->dispatch('toast', [
+        Component::macro('information', fn($message, $title = '', $delay = 5000) => $this->dispatch('toast', [
             'type' => ToastTypeEnum::INFORMATION,
             'message' => $message,
+            'title' => $title,
+            'delay' => $delay
         ]));
     }
 }
